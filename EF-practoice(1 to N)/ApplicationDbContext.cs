50e57858a63c99f1d321e25729169309a056d311
Wafaa Mohamed <121331529+Wafaamohamed2@users.noreply.github.com>
2024-09-14 20:18:11 +0300

    Add project files.

---
+++ EF-practoice(1 to N)/ApplicationDbContext.cs
@@ -0,0 +1,117 @@
+﻿
+using EF_practoice_1_to_N_;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEF
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        
+            options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EFCore; Integrated Security = True");
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //    define one to Many relation using flount API
+
+            //modelBuilder.Entity<Blog>()
+            //.HasMany(b => b.Posts)
+            //.WithOne();
+
+
+            // Or
+
+            ////modelBuilder.Entity<Post>()
+            ////    .HasOne(p => p.Blog)
+            ////    .WithMany(b => b.Posts);
+
+
+            //Or whene there are no navigation property in any classes
+            // we will use this
+
+            //modelBuilder.Entity<Post>()
+            //    .HasOne<Blog>()
+            //    .WithOne()
+            //    .HasForeignKey(p => p.BlogId)
+            //    ;
+
+            modelBuilder.Entity<RecordOfSafe>() 
+                .HasOne(s => s.Car)
+                .WithMany(c => c.SaleHistory)
+                .HasForeignKey(s => new {s.CarLicensePlate , s.CarState})
+                .HasPrincipalKey(c => new { c.LicensePalate  , c.State});
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+
+
+        public DbSet<RecordOfSafe> Records { get; set; }
+        public DbSet<Car> Cars { get; set; }
+    }
+
+    // One to Many relation
+
+    //Principal Entity 
+    public class Blog
+    {
+        public int BlogId { get; set; }  // PK
+
+        [Required , MaxLength(1000)]
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }    // Collection Navigation Property ----> which type is the type of tha Child
+
+
+    }
+
+    //Dependent Entity
+    public class Post
+    {
+      public int PostId { get; set; }   
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }   // FK
+
+        public Blog Blog {  get; set; }   // Reference Navigation Property ----> which type is the type of tha parent
+
+    }
+
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string LicensePalate { get; set; }
+
+        public string State { get; set; }
+        public string Moke { get; set; }
+
+        public List<RecordOfSafe> SaleHistory { get; set; }
+
+
+    }
+    public class RecordOfSafe
+    {
+
+        public int RecordOfSafeId { get; set; }
+        public DateTime DateSold { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string CarLicensePlate { get; set; }
+
+        public string CarState { get; set; }
+        public Car Car { get; set; }
+    }
+}
---
+++ EF-practoice(1 to N)/Migrations/20240914171432_CarsRel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_practoice_1_to_N_.Migrations
+{
+    /// <inheritdoc />
+    public partial class CarsRel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LicensePalate = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Moke = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.CarId);
+                    table.UniqueConstraint("AK_Cars_LicensePalate_State", x => new { x.LicensePalate, x.State });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Records",
+                columns: table => new
+                {
+                    RecordOfSafeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateSold = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CarLicensePlate = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CarState = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Records", x => x.RecordOfSafeId);
+                    table.ForeignKey(
+                        name: "FK_Records_Cars_CarLicensePlate_CarState",
+                        columns: x => new { x.CarLicensePlate, x.CarState },
+                        principalTable: "Cars",
+                        principalColumns: new[] { "LicensePalate", "State" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Records_CarLicensePlate_CarState",
+                table: "Records",
+                columns: new[] { "CarLicensePlate", "CarState" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Records");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
